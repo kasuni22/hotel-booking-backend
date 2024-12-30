c9d32a18b6cdafabcd2e53b653550460f1aa3d31
kasuni22 <kmkariyawasam4@gmail.com>
2024-12-30 22:28:14 +0530

    Set up room routes to handle CRUD operations

---
+++ routes/roomRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createRoom, deleteRoom, findRoomById, getRooms, updateRoom } from '../controllers/roomControllers.js';
+
+const roomRouter = express.Router();
+
+roomRouter.post("/", createRoom)
+roomRouter.delete("/:roomId", deleteRoom)
+roomRouter.get("/", getRooms)
+roomRouter.get("/:roomId", findRoomById)
+roomRouter.put("/:roomId", updateRoom)
+
+export default roomRouter
